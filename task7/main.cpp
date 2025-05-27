544147360cb46d1caeee6ab0b181ae3f88c6c5d3
SampiNova <dmitry.karpachev@hotmail.com>
2025-05-27 09:37:09 +0000

    new

--- task7/main.cpp
+++ task7/main.cpp
@@ -6,14 +6,12 @@
 #include <boost/program_options.hpp>
 #include <iomanip>
 #include <openacc.h>
-#include <nvtx3/nvToolsExt.h>
 #include <cublas_v2.h>
-#include <cuda_runtime.h>
+#include <nvtx3/nvToolsExt.h>
 
 #define OFFSET(row, col, m) (((row) * (m)) + (col))
 
 namespace po = boost::program_options;
-
 
 void write_to_file(const double* arr, int size, std::string filename)
 {
@@ -35,29 +33,47 @@ void write_to_file(const double* arr, int size, std::string filename)
     outputFile.close();
 }
 
-double calc_error_cublas(const double* d_u1, const double* d_u2, double* d_diff, int N, cublasHandle_t handle)
+void convolve_steps(double *restrict u_1, double *restrict u_2, int size, int steps)
 {
-    int size = N * N;
-    /*
-    double* d_diff = nullptr;
-    cudaMalloc((void**)&d_diff, size * sizeof(double));
-    */
+    for (int k = 0; k < steps; ++k)
+    {
+        #pragma acc kernels loop independent collapse(2) present(u_1, u_2) async
+        for (int i = 1; i < size - 1; ++i)
+        {
+            for (int j = 1; j < size - 1; ++j)
+            {
+                u_2[OFFSET(i, j, size)] = 0.25 * (u_1[OFFSET(i + 1, j, size)] + 
+                                              u_1[OFFSET(i - 1, j, size)] + 
+                                              u_1[OFFSET(i, j + 1, size)] + 
+                                              u_1[OFFSET(i, j - 1, size)]);
+            }
+        }
+        std::swap(u_1, u_2);
+    }
+    #pragma acc wait
+}
 
-    const double alpha = -1.0;
-    cudaMemcpy(d_diff, d_u2, size * sizeof(double), cudaMemcpyDeviceToDevice);
-    cublasDaxpy(handle, size, &alpha, d_u1, 1, d_diff, 1);
+double cublas_diff(cublasHandle_t h, double *restrict u_1, double *restrict u_2, double *restrict diff, int size)
+{
+    double alpha = -1.0;
 
-    int idx = 0;
-    cublasIdamax(handle, size, d_diff, 1, &idx);
+    int max_idx = 0;
+    #pragma acc host_data use_device(u_1, u_2, diff)
+    {
+        cublasDcopy(h, size * size, u_2, 1, diff, 1);
+        cublasDaxpy(h, size * size, &alpha, u_1, 1, diff, 1);
+        cublasIdamax(h, size * size, diff, 1, &max_idx);
+    }
+    #pragma acc update host(diff[max_idx - 1])
 
-    idx = idx - 1;
+    return fabs(diff[max_idx - 1]);
+}
 
-    double max_err = 0.0;
-    cudaMemcpy(&max_err, d_diff + idx, sizeof(double), cudaMemcpyDeviceToHost);
-    max_err = std::abs(max_err);
-
-    // cudaFree(d_diff);
-    return max_err;
+double error_step(cublasHandle_t h, double *restrict u_1, double *restrict u_2, double *restrict diff, int size, int steps)
+{
+    convolve_steps(u_1, u_2, size, steps);
+    
+    return cublas_diff(h, u_1, u_2, diff, size);
 }
 
 int main(int argc, char *argv[])
@@ -78,10 +94,12 @@ int main(int argc, char *argv[])
     double tolerance = vm["tolerance"].as<double>();
     int max_iterations = vm["max_iterations"].as<int>();
 
-    std::cout << "Matrix size" << N << "x" << N << std::endl;
+    acc_set_device_num(2, acc_device_nvidia);
+
+    std::cout << "Matrix size: " << N << "x" << N << std::endl;
     std::cout << "Tolerance: " << tolerance << std::endl;
     std::cout << "Maximum iterations: " << max_iterations << std::endl;
-
+    
     acc_device_t dev_type = acc_get_device_type();
     int dev_num = acc_get_device_num(dev_type);
 
@@ -100,91 +118,65 @@ int main(int argc, char *argv[])
 
     std::unique_ptr<double[]> u_1_unique(std::make_unique<double[]>(N * N));
     std::unique_ptr<double[]> u_2_unique(std::make_unique<double[]>(N * N));
-    std::unique_ptr<double[]> u_diff_unique(std::make_unique<double[]>(N * N));
+    std::unique_ptr<double[]> diff_unique(std::make_unique<double[]>(N * N));
 
     double corner1 = 10.0;
     double corner2 = 20.0;
     double corner3 = 30.0;
     double corner4 = 20.0;
 
-    nvtxRangePushA("init");
-    for (int i = 0; i < N; ++i)
-    {
-        u_1_unique[OFFSET(0, i, N)] = corner1 + (corner2 - corner1) * i / (N - 1.0);
-        u_1_unique[OFFSET(N - 1, i, N)] = corner4 + (corner3 - corner4) * i / (N - 1.0);
-        u_1_unique[OFFSET(i, 0, N)] = corner1 + (corner4 - corner1) * i / (N - 1.0);
-        u_1_unique[OFFSET(i, N - 1, N)] = corner2 + (corner3 - corner2) * i / (N - 1.0);
-
-        u_2_unique[OFFSET(0, i, N)] = corner1 + (corner2 - corner1) * i / (N - 1.0);
-        u_2_unique[OFFSET(N - 1, i, N)] = corner4 + (corner3 - corner4) * i / (N - 1.0);
-        u_2_unique[OFFSET(i, 0, N)] = corner1 + (corner4 - corner1) * i / (N - 1.0);
-        u_2_unique[OFFSET(i, N - 1, N)] = corner2 + (corner3 - corner2) * i / (N - 1.0);
-    }
-    nvtxRangePop();
-
-    int iteration = 0;
-    double error = 1.0;
-    
-    auto start = std::chrono::high_resolution_clock::now();
     double* u_1 = u_1_unique.get();
     double* u_2 = u_2_unique.get();
-    double* u_diff = u_diff_unique.get();
+    double* diff = diff_unique.get();
     
-    nvtxRangePushA("while_with_copy");
-    #pragma acc data copyin(error, u_diff[0:N*N], u_1[0:N*N], u_2[0:N*N])
-    {
-        cublasHandle_t handle;
-        cublasCreate(&handle);
+    nvtxRangePushA("init");
+    std::memset(u_1, 0, N * N * sizeof(double));
+    std::memset(u_2, 0, N * N * sizeof(double));
+    std::memset(diff, 0, N * N * sizeof(double));
 
+    for (int i = 0; i < N; ++i)
+    {
+        u_1[OFFSET(0, i, N)] = corner1 + (corner2 - corner1) * i / (N - 1.0);
+        u_1[OFFSET(N - 1, i, N)] = corner4 + (corner3 - corner4) * i / (N - 1.0);
+        u_1[OFFSET(i, 0, N)] = corner1 + (corner4 - corner1) * i / (N - 1.0);
+        u_1[OFFSET(i, N - 1, N)] = corner2 + (corner3 - corner2) * i / (N - 1.0);
+
+        u_2[OFFSET(0, i, N)] = corner1 + (corner2 - corner1) * i / (N - 1.0);
+        u_2[OFFSET(N - 1, i, N)] = corner4 + (corner3 - corner4) * i / (N - 1.0);
+        u_2[OFFSET(i, 0, N)] = corner1 + (corner4 - corner1) * i / (N - 1.0);
+        u_2[OFFSET(i, N - 1, N)] = corner2 + (corner3 - corner2) * i / (N - 1.0);
+    }
+    nvtxRangePop();
+    
+    cublasHandle_t handle;
+    cublasCreate(&handle);
+
+    int iteration = 0;
+    
+    std::cout << "Start calculations.\n";
+    auto start = std::chrono::high_resolution_clock::now();
+    
+    #pragma acc data copyin(u_1[0:N*N], u_2[0:N*N]) create(diff[0:N*N])
+    {
+        double error = tolerance + 1.0;
         nvtxRangePushA("while");
         while (iteration < max_iterations && error > tolerance)
         {
-            nvtxRangePushA("calc_step");
-            #pragma acc parallel loop independent collapse(2) present(u_1, u_2) // async(1)
-            for (int i = 1; i < N - 1; ++i)
-            {
-                for (int j = 1; j < N - 1; ++j)
-                {
-                    u_2[OFFSET(i, j, N)] = 0.25 * (u_1[OFFSET(i + 1, j, N)] + u_1[OFFSET(i - 1, j, N)] + u_1[OFFSET(i, j + 1, N)] + u_1[OFFSET(i, j - 1, N)]);
-                }
-            }
+            nvtxRangePushA("step");
+            error = error_step(handle, u_1, u_2, diff, N, error_calc_step);
+            iteration += error_calc_step;
             nvtxRangePop();
-            
-            if ((iteration + 1) % error_calc_step == 0)
-            {
-                nvtxRangePushA("calc_error");
-                double* d_u1 = (double*)acc_deviceptr(u_1);
-                double* d_u2 = (double*)acc_deviceptr(u_2);
-                double* d_diff = (double*)acc_deviceptr(u_diff);
-                error = calc_error_cublas(d_u1, d_u2, d_diff, N, handle);
-                std::cout << iteration << " " << error << "\n";
-                nvtxRangePop();
-            }
-            
-            nvtxRangePushA("swap u_1 u_2");
-            /*
-            #pragma acc parallel loop independent collapse(2) present(u_1, u_2)
-            for (int i = 1; i < N - 1; ++i)
-                for (int j = 1; j < N - 1; ++j)
-                    u_1[OFFSET(i, j, N)] = u_2[OFFSET(i, j, N)];
-            */
-            std::swap(u_1, u_2);
-            nvtxRangePop();
-
-            ++iteration;
         }
-        #pragma acc update self(u_1[0:N*N])
+        std::cout << "Error: " << error << "\n";
         nvtxRangePop();
-        cublasDestroy(handle);
+        #pragma acc update host(u_1[0:N*N])
     }
-    nvtxRangePop();
 
     auto end = std::chrono::high_resolution_clock::now();
     std::chrono::duration<double> duration = end - start;
     auto time = std::chrono::duration_cast<std::chrono::seconds>(duration).count();
 
     std::cout << "Total iterations: " << iteration << std::endl;
-    std::cout << "Final error: " << error << std::endl;
     std::cout << "Time: " << duration.count() << "\n";
     
     write_to_file(u_1, N, "res_128.txt");
